e87c5cf7ceefe880f1d2761b63db1d2a274d0f92
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-21 02:17:39 +0300

    task-02 complite

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+
+
+
+console.log(ingredients);
+
+const list = document.querySelector("#ingredients");
+console.log(list);
+
+ingredients.forEach(function (ingredients) {
+
+  const ingredientsList = document.createElement("li"); //создаем li для каждого елемента.
+  ingredientsList.textContent = ingredients;            //присваиваем название из массива.
+  ingredientsList.classList.add("item");                // добавляем class item.
+  list.append(ingredientsList);                         //добавляем елементы после #ingredients.
+
+  console.log(ingredientsList);
+});
