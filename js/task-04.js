39fa7c35f5b0f7c317696f4bf32bd16ac3732995
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-21 18:45:02 +0300

    task-04 comlite

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+//доступ к button
+const decBtn = document.querySelector('[data-action="decrement"]');
+const incBtn = document.querySelector('[data-action="increment"]');
+//доступ к value
+const valueId = document.querySelector("#value");
+
+let counterValue = 0;
+
+decBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  //innerHTML вернули значение в HTML
+  valueId.innerHTML = counterValue;
+});
+
+incBtn.addEventListener("click", () => {
+  counterValue += 1;
+  valueId.innerHTML = counterValue;
+});
