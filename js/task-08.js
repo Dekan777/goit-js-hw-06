3d6dde6c94b71c8793ba9cf4afe91f9f66328d96
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-23 00:19:49 +0300

    task-08 complite

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+//скрипт управления формой логина
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = registerForm.elements.email.value;
+  const password = registerForm.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Fill in all the fields!");
+  }
+  alert(`email: ${this.email.value}, password: ${this.password.value}`);
+}
