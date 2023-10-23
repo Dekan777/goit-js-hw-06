f94e05b6d7a729f4b74c01f07398e912b9c05f1b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-10-23 17:29:56 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,21 +1,16 @@
 const elementButton = document.querySelector(".change-color");
-// console.log("elementButton", elementButton);
 elementButton.addEventListener("click", changeColor);
 
 function changeColor() {
   const randomColor = getRandomHexColor();
-  // console.log(randomColor);
 
   //получение ссылки на первый елемент колекции тега body
   const elementBody = document.getElementsByTagName("body")[0];
-  // console.log(elementBody);
-
+  
   //получение ссыылки елемента span
   const elementSpan = document.querySelector(".color");
-  // console.log(elementSpan);
 
   elementBody.style.backgroundColor = randomColor;
-
   elementSpan.textContent = randomColor;
 }
 
